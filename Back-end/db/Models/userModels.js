43154e81119c31c008fd78f67862cb175c1b78f5
Bouguerra Med.Amine <132056227+BouguerraMohamedAmine@users.noreply.github.com>
2023-08-28 13:32:05 +0100

    Back-end done !!!

---
+++ Back-end/db/Models/userModels.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  image: String,
+  phoneNumber: String,
+  city: String,
+  reviewedProducts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  }],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ Back-end/db/Routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const productController = require('../Controllers/productControllers'); // Corrected path
+
+const router = express.Router();
+
+router.post('/products', productController.createProduct); // Corrected function name
+router.get('/products', productController.getProducts); // Corrected function name
+router.get('/products/:id', productController.getProductById); // Corrected function name
+router.put('/products/:id', productController.updateProduct); // Corrected function name
+router.delete('/products/:id', productController.deleteProduct); // Corrected function name
+router.get('/products/reviews', productController.getProductsByReviews); // Corrected function name
+router.get('/products/name/:name', productController.getProductByName); // Corrected function name
+
+module.exports = router;
